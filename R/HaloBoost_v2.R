9793d6d0ddeafa1d1c66eb1779915f28a2be7dc0
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-27 11:08:32 +1300

    awd

--- R/HaloBoost_v2.R
+++ R/HaloBoost_v2.R
@@ -77,7 +77,7 @@ forecast.rf <- function(train, test, Projection, proj_data) {
   
 }
 
-forecast.xgboost <- function (train, test, Projection, proj_data,feature.names,eta,max_depth_xgb) {
+forecast.xgboost <- function (train, test, Projection, proj_data,feature.names,eta,max_depth_xgb,nrounds) {
   # Define error function
   tra <-train[,feature.names]
   RMPSE <- function(preds, dtrain) {
@@ -636,7 +636,7 @@ forecasting <- function(configFile,inputFile){
         test = test,
         Projection = projection,
         proj_data = project,
-        feature.names, eta,max_depth_xgb
+        feature.names, eta,max_depth_xgb, nrounds
         
       )
       # , grid_search = grid_search)
