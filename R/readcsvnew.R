61a381b8a5744b82e69a1700510819a24055cdc8
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-26 15:33:43 +1300

    last try i promise

--- R/readcsvnew.R
+++ R/readcsvnew.R
@@ -6,27 +6,20 @@
 #' @param file a csv file.
 #' @param fileinput a csv file.
 
-config <- function(file){
-  if(!grepl(".csv$", file)){
-    stop("Uploaded file must be a .csv file!")
-  }
-  read.csv(
-    configFile,
-    skipNul = T,
-    colClasses = "character"
-  )
- 
-}
+
+config <-  read.csv(
+  file,
+  skipNul = T,
+  colClasses = "character"
+)
+
 print(config)
-input <- function(fileinput){
-  if(!grepl(".csv$", fileinput)){
-    stop("Uploaded file must be a .csv file!")
-  }
-  read.csv(
-    fileinput,
-    skipNul = T,
-    colClasses = "character"
-  )
-}
+
+input <-  read.csv(
+  fileinput,
+  skipNul = T,
+  colClasses = "character"
+)
+
 
 print(summary(input))
