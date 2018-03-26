ebbd20d600babcb1f245f40d6fc5aeedf18f42dd
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-26 15:17:28 +1300

    First try

--- R/readcsvnew.R
+++ R/readcsvnew.R
@@ -4,10 +4,28 @@
 #' 
 #' @export
 #' @param file a csv file.
+#' @param fileinput a csv file.
 #' @param ... arguments passed to read.csv
-readcsvnew <- function(file, ...){
+conifg <- function(file, ...){
   if(!grepl(".csv$", file)){
     stop("Uploaded file must be a .csv file!")
   }
-  read.csv(file, ...);
+  read.csv(
+    configFile,
+    skipNul = T,
+    colClasses = "character"
+  )
 }
+print(config)
+input <- function(fileinput, ...){
+  if(!grepl(".csv$", fileinput)){
+    stop("Uploaded file must be a .csv file!")
+  }
+  read.csv(
+    fileinput,
+    skipNul = T,
+    colClasses = "character"
+  )
+}
+
+print(summary(input))
