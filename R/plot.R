a5602c9b9acd9e0fa3bd6f9c7d1e377aaec36f32
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-28 16:55:09 +1300

    ahahahha

--- R/plot.R
+++ R/plot.R
@@ -3,13 +3,13 @@
 #' @export
 #' @param mydata some object or dataset
 plot_forecasting <- function(mydata){
-  months = c ("1","2","3","4","5","6","7","8","9","10","11","12")
-  value1 = c(1,2,3,4,5,6,7,8,9,10,11,12)
-  value2 = c(2,4,6,8,10,12,14,16,18,20,22,24)
+  # months = c ("1","2","3","4","5","6","7","8","9","10","11","12")
+  # value1 = c(1,2,3,4,5,6,7,8,9,10,11,12)
+  # value2 = c(2,4,6,8,10,12,14,16,18,20,22,24)
   
-  myvector = c(value1,value2)
+  # myvector = c(value1,value2)
 
-  myts <- ts(myvector, start=c(2016, 1), end=c(2017, 12), frequency=12)
+  myts <- ts(mydata, start=c(2016, 1), end=c(2017, 12), frequency=12)
   print(plot(forecast(myts)))
 }
 
--- R/printsummary.R
+++ R/printsummary.R
@@ -5,5 +5,6 @@
 #' @export
 #' @param mydata some object or dataset
 printsummary <- function(mydata){
-  print(mydata)
+  myts <- ts(mydata, start=c(2016, 1), end=c(2017, 12), frequency=12)
+  print(forecast(myts))
 }
