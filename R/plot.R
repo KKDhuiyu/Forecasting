9d85d2d0a087d0c26541a19c0f35c43338c4f999
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-29 09:49:21 +1300

    pfft

--- R/plot.R
+++ R/plot.R
@@ -5,7 +5,7 @@
 #' @param algo the forecasting fucntion to use
 #' 
 #' 
-plot_forecasting <- function(mydata){
+plot_forecasting <- function(mydata,algo){
   # months = c ("1","2","3","4","5","6","7","8","9","10","11","12")
   # value1 = c(1,2,3,4,5,6,7,8,9,10,11,12)
   # value2 = c(2,4,6,8,10,12,14,16,18,20,22,24)
@@ -19,7 +19,7 @@ plot_forecasting <- function(mydata){
     install.packages("forecast")
   }
   library(forecast)
-  algorithm = message = paste(algorithm, "", R.Version()$version.string)
+  algorithm = message = paste(algo, "", R.Version()$version.string)
   myts <- ts(mydata, start=c(2016, 1), end=c(2017, 12), frequency=12)
   if(algorithm == "ets"){
     print(plot(forecast(ets(myts))))
