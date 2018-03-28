ec544cb24b68269f298666a4aa7005d60e1ec286
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-28 16:40:30 +1300

    update

--- R/plot.R
+++ R/plot.R
@@ -8,8 +8,7 @@ plot_forecasting <- function(mydata){
   value2 = c(2,4,6,8,10,12,14,16,18,20,22,24)
   
   myvector = c(value1,value2)
-  a = ausbeer
-  typeof(ausbeer)
+
   myts <- ts(myvector, start=c(2016, 1), end=c(2017, 12), frequency=12)
   print(plot(forecast(myts)))
 }
