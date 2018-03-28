69facefc64f2a638a0d70c5b19b80b7aa64d514d
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-28 16:22:37 +1300

    wd

--- R/plot.R
+++ R/plot.R
@@ -3,5 +3,15 @@
 #' @export
 #' @param mydata some object or dataset
 plot_forecasting <- function(mydata){
-  print(mydata)
+  months = c ("1","2","3","4","5","6","7","8","9","10","11","12")
+  value1 = c(1,2,3,4,5,6,7,8,9,10,11,12)
+  value2 = c(2,4,6,8,10,12,14,16,18,20,22,24)
+  
+  myvector = c(value1,value2)
+  a = ausbeer
+  typeof(ausbeer)
+  myts <- ts(myvector, start=c(2016, 1), end=c(2017, 12), frequency=12)
+  print(plot(forecast(myts)))
 }
+
+
