031af8ddcd3158851dcce55d49a450dfe801af58
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-28 17:01:10 +1300

    awd

--- R/printsummary.R
+++ R/printsummary.R
@@ -15,5 +15,6 @@ printsummary <- function(mydata){
   }
   library(forecast)
   myts <- ts(mydata, start=c(2016, 1), end=c(2017, 12), frequency=12)
-  print(forecast(myts))
+  # print(forecast(myts))
+  print(myts)
 }
