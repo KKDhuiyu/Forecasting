2c79666ee2416945e74e90c2ed19383269ec5069
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-28 15:16:44 +1300

    build

--- R/printsummary.R
+++ R/printsummary.R
@@ -5,11 +5,5 @@
 #' @export
 #' @param mydata some object or dataset
 printsummary <- function(mydata){
-  #override default 
-  options(max.print=99999999);
-  options(width=120);
-  
-  #print
   print(summary(mydata))
-  invisible()
 }
