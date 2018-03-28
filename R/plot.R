1e5cd3d1ed95d6e3293958a2b2760bad0c5ccefe
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-28 16:58:16 +1300

    wdwdwd

--- R/plot.R
+++ R/plot.R
@@ -8,7 +8,14 @@ plot_forecasting <- function(mydata){
   # value2 = c(2,4,6,8,10,12,14,16,18,20,22,24)
   
   # myvector = c(value1,value2)
-
+  if (!require("ggplot2")) {
+    install.packages("ggplot2")
+  }
+  library(ggplot2)
+  if (!require("forecast")) {
+    install.packages("forecast")
+  }
+  library(forecast)
   myts <- ts(mydata, start=c(2016, 1), end=c(2017, 12), frequency=12)
   print(plot(forecast(myts)))
 }
