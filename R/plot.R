3016cfefbd380bf53fc3003f4451b5518ba11729
Huiyu Jia <jiahuiyu0328@gmail.com>
2018-03-29 09:41:17 +1300

    wad

--- R/plot.R
+++ R/plot.R
@@ -2,6 +2,7 @@
 #' 
 #' @export
 #' @param mydata some object or dataset
+#' @param algorithm the forecasting fucntion to use
 plot_forecasting <- function(mydata){
   # months = c ("1","2","3","4","5","6","7","8","9","10","11","12")
   # value1 = c(1,2,3,4,5,6,7,8,9,10,11,12)
@@ -17,7 +18,13 @@ plot_forecasting <- function(mydata){
   }
   library(forecast)
   myts <- ts(mydata, start=c(2016, 1), end=c(2017, 12), frequency=12)
-  print(plot(forecast(myts)))
+  if(algorithm == "ets"){
+    print(plot(forecast(ets(myts))))
+  }else if(algorithm == "ARIMA"){
+    print(plot(forecast(auto.arima(myts))))
+  }else{
+    print(plot(forecast(myts)))
+  }
 }
 
 
